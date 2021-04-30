cf05714dd906a1bbba7c88bc2af897656da21cfb
Nina Kim <neenerskim@gmail.com>
2021-04-30 11:20:06 -0700

    set background color and more recipe info

--- src/components/RecipeBody/RecipeBody.jsx
+++ src/components/RecipeBody/RecipeBody.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Ingredients from '../../components/Ingredients/Ingredients'
 import Instructions from '../../components/Instructions/Instructions'
-import {  Divider, Segment } from 'semantic-ui-react'
+import {  Divider, Segment, Header, Grid } from 'semantic-ui-react'
 
 export default function RecipeBody({ recipe }){
     return (
         <>
         <Segment padded>
+            <Grid columns={2} textAlign='center'>
+                <Grid.Column>
+                    <Header><em>Cuisine:  {recipe.cuisine}</em></Header>
+                </Grid.Column>
+                <Grid.Column>
+                    <Header><em>Cook Time:  {recipe.cookTime}</em></Header>
+                </Grid.Column>
+            </Grid>
+            {/* <Divider hidden/> */}
             <Ingredients recipe={recipe}/>
             <Divider hidden/>
             <Instructions recipe={recipe}/>           
