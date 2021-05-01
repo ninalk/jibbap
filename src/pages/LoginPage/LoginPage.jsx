fdab259c943c7494db65d883951c8f23f9629159
Nina Kim <neenerskim@gmail.com>
2021-05-01 13:38:19 -0700

    add logo to login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
-import loginpage from '../../images/kimbap.png';
+import loginpage from '../../images/JJ_icon_white-01.png';
+import kimbap from '../../images/kimbap.png';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 import userService from '../../utils/userService';
 import { useHistory, Link } from 'react-router-dom';
@@ -38,48 +39,58 @@ export default function LoginPage(props){
     return (
         <>
             <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                <Image 
-                    style={{ maxWidth: 300 }}
-                    src={loginpage} 
-                    className="six wide column image signup-image"
-                    verticalAlign='middle'
-                />
-                <Form  autoComplete="off"  onSubmit={handleSubmit} style={{ maxWidth: 400 }} className="six wide column">
-                    <Header as='h2' textAlign='center' className="signup-logo">
-                    JIBBAP JOURNEY
-                    </Header>
-                    <Segment stacked>
-                        <Form.Input
-                        type="email"
+                <Grid.Column width={3}>
+                    <Image 
+                        src={kimbap} 
+                        className="image signup-image"
+                        size='medium'
+                    />
+                </Grid.Column>
+                <Grid.Column style={{ maxWidth: 400 }} width={6}>
+                    <Form  autoComplete="off" onSubmit={handleSubmit} >
                         
-                        name="email"
-                        placeholder="email"
-                        value={ state.email}
-                        onChange={handleChange}
-                        required
-                        />
-                        <Form.Input
-                        name="password"
-                        type="password"
-                        placeholder="password"
-                        value={ state.password}
-                        onChange={handleChange}
-                        required
-                        />
-                    <Button
-                        color='teal'
-                        fluid size='large'
-                        type="submit"
-                        className="btn"
-                        // disabled={invalidForm}
-                    >
-                        Login
-                    </Button>
-                    <Message>
-                        New to us? <Link to='/signup'>&nbsp;Sign Up</Link>
-                    </Message>
-                    </Segment>
-                </Form>
+                        <Header as='h2' textAlign='center' className="signup-logo">
+                            <Image 
+                                src={loginpage} 
+                                className="image signup-image"
+                                verticalAlign='middle'
+                                size='medium'
+                            />
+                            JIBBAP JOURNEY
+                        </Header>
+                        <Segment stacked>
+                            <Form.Input
+                            type="email"
+                            
+                            name="email"
+                            placeholder="email"
+                            value={ state.email}
+                            onChange={handleChange}
+                            required
+                            />
+                            <Form.Input
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            value={ state.password}
+                            onChange={handleChange}
+                            required
+                            />
+                        <Button
+                            color='teal'
+                            fluid size='large'
+                            type="submit"
+                            className="btn"
+                            // disabled={invalidForm}
+                        >
+                            Login
+                        </Button>
+                        <Message>
+                            New to us? <Link to='/signup'>&nbsp;Sign Up</Link>
+                        </Message>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
                 {error ? <ErrorMessage error={error} /> : null}
             </Grid>
       </>
